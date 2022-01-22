0001464a5891232812bafaf346a569566486708a
Диляра Садыкова <dilyara.mozhet.sadykova@gmail.com>
2022-01-22 13:23:02 +0300

    Добавьте файлы проекта.

---
+++ WpfLab_17_2/ColorControl.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLab_17_2
+{
+    /// <summary>
+    /// Логика взаимодействия для ColorControl.xaml
+    /// </summary>
+    public partial class ColorControl : UserControl
+    {
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(
+                nameof(Color),
+                typeof(Color),
+                typeof(ColorControl),
+                new FrameworkPropertyMetadata(
+                    Colors.Black,
+                    0,
+                    new PropertyChangedCallback(OnColorChanged)));
+        public static readonly DependencyProperty RedProperty =
+            DependencyProperty.Register(
+                nameof(Red),
+                typeof(byte),
+                typeof(ColorControl),
+                new FrameworkPropertyMetadata(
+                    default(byte),
+                    0,
+                    new PropertyChangedCallback(OnColorRGBChanged)
+                    ));
+        public static readonly DependencyProperty GreenProperty =
+            DependencyProperty.Register(
+                nameof(Green),
+                typeof(byte),
+                typeof(ColorControl),
+                new FrameworkPropertyMetadata(
+                    default(byte),
+                    0,
+                    new PropertyChangedCallback(OnColorRGBChanged)
+                    ));
+        public static readonly DependencyProperty BlueProperty =
+            DependencyProperty.Register(
+                nameof(Blue),
+                typeof(byte),
+                typeof(ColorControl),
+                new FrameworkPropertyMetadata(
+                    default(byte),
+                    0,
+                    new PropertyChangedCallback(OnColorRGBChanged)
+                    ));
+
+        public static readonly RoutedEvent ColorChangedEvent =
+            EventManager.RegisterRoutedEvent(
+                "ColorChanged",
+                RoutingStrategy.Bubble,
+                typeof(RoutedPropertyChangedEventHandler<Color>),
+                typeof(ColorControl));
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+        public byte Red
+        {
+            get { return (byte)GetValue(RedProperty); }
+            set { SetValue(RedProperty, value); }
+        }
+        public byte Green
+        {
+            get { return (byte)GetValue(GreenProperty); }
+            set { SetValue(GreenProperty, value); }
+        }
+        public byte Blue
+        {
+            get { return (byte)GetValue(BlueProperty); }
+            set { SetValue(BlueProperty, value); }
+        }
+
+        private static void OnColorRGBChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            ColorControl colorControl = (ColorControl)sender;
+            Color color = colorControl.Color;
+            if (e.Property == RedProperty)
+                color.R = (byte)e.NewValue;
+            else if (e.Property == GreenProperty)
+                color.G = (byte)e.NewValue;
+            else if (e.Property == BlueProperty)
+                color.B = (byte)e.NewValue;
+
+            colorControl.Color = color;
+        }
+
+        private static void OnColorChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Color newColor = (Color)e.NewValue;
+            ColorControl colorControl = (ColorControl)sender;
+            colorControl.Red = newColor.R;
+            colorControl.Green = newColor.G;
+            colorControl.Blue = newColor.B;
+        }
+
+        public event RoutedPropertyChangedEventHandler<Color> ColorChanged
+        {
+            add { AddHandler(ColorChangedEvent, value); }
+            remove { RemoveHandler(ColorChangedEvent, value); }
+        }
+
+        public ColorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
